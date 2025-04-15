bdc941119154fca8aba9687944ed7b5535398d9e
Monishka khanna <khannatinkle191@gmail.com>
2025-04-15 18:54:55 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all of js as newer version 
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3) // code readability should be high 
+
+console.log("monishka")
+
+let name = "monishka"
+let age = 17 
+let isloggedin = true
+
+//numbers => 2 to power 53
+//bigint
+//string =>""
+//boolean => true,false 
+//null => standalone value 
+//undefined =>
+//symbols => unique 
+
+
+//object
+
+console.log(typeof "monishka")
