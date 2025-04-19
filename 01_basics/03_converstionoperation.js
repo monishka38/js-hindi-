97f1778399e6f2a3c85236a196d2a32108ba61c4
Monishka khanna <khannatinkle191@gmail.com>
2025-04-19 13:40:58 +0000

    convertion of datatypes

---
+++ 01_basics/03_converstionoperation.js
@@ -0,0 +1,28 @@
+let score = false
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abs" => Nan
+// true => 1; false => 0 
+
+let isloggedIn = 1
+
+let booleanisloggedIn = Boolean(isloggedIn)
+//console.log(booleanisloggedIn)
+
+//1 => true; 0=> false 
+// "" => false 
+// "monishka" => true
+
+let someNumber = 38
+
+let stringNumber = String(someNumber)
+console.log(typeof (stringNumber));
+console.log(stringNumber)
