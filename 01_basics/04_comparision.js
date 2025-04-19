552506fa8a0c7191585c5822497d6092eaded702
Monishka khanna <khannatinkle191@gmail.com>
2025-04-19 14:36:11 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,28 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2==1);
+
+//console.log("2"> 1);
+//console.log("02">1);
+
+//console.log(null > 0);
+//console.log(null==0);
+//console.log(null >=0);
+
+// in js null sometime behave as zero so outcome came as true 
+
+console.log(undefined >0);
+console.log(undefined ==0);
+console.log(undefined >= 0);
+
+//undefined always show false when compaired to any thing
+//=== 
+
+console.log("2" === 2);
+
+
+
+
+
+
