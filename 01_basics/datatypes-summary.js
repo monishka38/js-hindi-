a99702231a7ac3b5ec0e94405a2c1e2b8198cb99
Monishka khanna <khannatinkle191@gmail.com>
2025-04-20 15:56:22 +0000

    datatypes

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -28,10 +28,34 @@ const myFunction = function(){
     console.log("hellow world");
 }
 
-console.log(typeof outsidetemp);
+//console.log(typeof outsidetemp);
 // type of 
 // null => object 
 //boolean => boolean 
 // string => string 
 // undefined => undefined 
 // number => number 
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//stack (primitive) ; heap (non primitive) 
+
+let myname = "monishka"
+
+let anothername = myname 
+anothername = "tinkle"
+
+console.log(myname);
+console.log(anothername);
+
+let userOne = {
+    Email : "monishka@gmail.com",
+    upi : "user@bly"
+}
+
+let userTwo = userOne 
+userTwo.Email = "tinkle@gmail.com"
+
+console.log(userOne.Email);
+console.log(userTwo.Email);
+
